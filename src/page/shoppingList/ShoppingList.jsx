7ba8e76f12adbe684c8c1a2e61ced54f2bbbd0bc
dgagan2 <andgag@duck.com>
2023-06-22 00:11:24 -0500

    Se modifica la pagina de lista de compra, se renderiza la informaciòn de los productos, valores y precio total

--- src/page/shoppingList/ShoppingList.jsx
+++ src/page/shoppingList/ShoppingList.jsx
@@ -1,51 +1,62 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable eqeqeq */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-indent */
-import React, { useState } from 'react'
+import React from 'react'
 import { useProductContext } from '@/hooks/UseProductContext'
 import Navbar from '@/components/navbar/Navbar'
+import quitar from '@/assets/quitar-del-carrito.png'
 const ShoppingList = () => {
-  const { shoppingList } = useProductContext()
-  const [checked, setChecked] = useState([])
-  const handleCheck = (event) => {
-    const id = event.target.value
-    if (event.target.checked) {
-      setChecked([...checked, id])
-    } else {
-      const newChecked = checked.filter((data) => data !== id)
-      setChecked(newChecked)
-    }
-  }
+  const { shoppingList, setShoppingList } = useProductContext()
+  let total = 0
+  shoppingList.forEach(item => {
+    const subtotal = item.price * item.cantidad
+    total += subtotal
+  })
 
-  const handleDelete = () => {
-    const newShoppingList = shoppingList.filter((product) => product.id !== checked)
+  const handleDelete = (id) => {
+    const newShoppingList = shoppingList.filter((product) => product.id != id)
+    setShoppingList(newShoppingList)
   }
   return (
     <>
       <Navbar />
       {Object.keys(shoppingList).length > 0
         ? <section style={{ padding: '70px 40px' }} className='listAddProducts'>
-          <div className='d-flex flex-row containerButtons'>
-            <button onClick={() => handleDelete()}>Eliminar</button>
-          </div>
-          <ul className='d-flex gap-4 flex-column'>
-            {shoppingList?.map((product) => (
-              <li key={product.id} style={{ listStyle: 'none' }}>
-                <article className='d-flex gap-3 align-items-center'>
-                  <input type='checkbox' value={product.id} onChange={(event) => handleCheck(event)} />
-                  <img style={{ width: '110px' }} src={product?.images[0]} />
-                  <div>
-                    <p>{product?.title}</p>
-                    <p>$ {product.price}</p>
-                    <input value={product.cantidad} />
-                  </div>
-                  <p>$ {product.price * product.cantidad}</p>
-                </article>
-              </li>
-            ))}
-          </ul>
+          <div className='table-responsive-md'>
+              <table className='table table-hover'>
+                <thead>
+                  <th scope='col'>Cantidad</th>
+                  <th scope='col'>Producto</th>
+                  <th scope='col'>Valor unitario</th>
+                  <th scope='col'>Valor por item</th>
+                  <th scope='col'>Quitar</th>
+                </thead>
+                <tbody>
+                  {shoppingList?.map((p) => (
+                    <tr key={p.id}>
+                      <td><input type='text' name='cantidad' id='cantidad' value={p.cantidad} /></td>
+                      <td>
+                        <div>
+                          <img style={{ width: '80px' }} src={p?.images} alt='' />
+                          <p>{p?.title}</p>
+                        </div>
+                      </td>
+                      <td>$ {p.price}</td>
+                      <td>$ {p.price * p.cantidad}</td>
+                      <td> <button className='iconQuitarProducto' onClick={() => handleDelete(p.id)}><img src={quitar} /></button></td>
+                    </tr>
+                  ))}
+                    <tr>
+                      <td colSpan='2' style={{ textAlign: 'end' }}>Total</td>
+                      <th scope='row'>$ {total}</th>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
           </section>
-        : <section className='d-flex flex-column gap-2' style={{ padding: '70px 40px' }}>
-          <b>No hay productos agregados al carro de compras, vuelve y seleciona uno </b>
+        : <section className='d-flex flex-column gap-2' style={{ padding: '70px 40px', textAlign: 'center' }}>
+          <b>No hay productos agregados al carro de compras, vuelva y seleciona uno </b>
         </section>}
 
     </>
